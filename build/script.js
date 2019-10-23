da7f661fe4118069549eaf0aa5f1609d05957f69
katrinLuna <kate.nik2017@yandex.ru>
2019-10-23 20:51:23 +0300

    create more tracket for btn

--- build/script.js
+++ build/script.js
@@ -1,21 +1,39 @@
-var settingBtnElement = document.querySelector('.card-market__settings');
-var activateToogleMarketElement = document.querySelector('.card-market__toogle-active-btn');
+var settingBtnElements = document.querySelectorAll('.card-market__settings');
+var activateToogleMarketElements = document.querySelectorAll('.card-market__toogle-active-btn');
 var stateMarketElement = document.querySelector('.card-market__state--creating');
 
-settingBtnElement.addEventListener('click', function (event) {
-        if (activateToogleMarketElement.style.display == 'none') {
-            activateToogleMarketElement.style.display = "block";
-            event.btnHandl = true;
+// settingBtnElements.addEventListener('click', function (event) {
+//         if (activateToogleMarketElement.style.display == 'none') {
+//             activateToogleMarketElement.style.display = "block";
+//             event.btnHandl = true;
+//         }
+//     }
+// );
+
+settingBtnElements.forEach(function(item, i) {
+
+    item.addEventListener('click', function (event) {
+    // добавить цикл в обработчик который будет скрыть все элементы кроме i
+        
+        if (activateToogleMarketElements[i].style.display == 'none') {
+            activateToogleMarketElements[i].style.display = "block";
+            event.btnHandl = true;            
+            event.bla = 1; //это если у нас два обработчика на одно событие - в плане ловим и на документе это событие - в параметр записываем i элемент и внутри сл обработчика делаем скрытие 
+            // -- если есть это свойство в объекте событие- значит у нас через кнопку событие прошло - и тогда у любого кроме i-элемента скрываем блок, а если нет свойства - то скрываем у всех
+            // можно в  btnHandl  прям класть цифру и ниже проверять, что тут нет null 
+            console.log(event);
         }
-    }
-);
+    });
+});
 
 document.addEventListener('click', function (event) {
-        if (!event.btnHandl) {       
-            activateToogleMarketElement.style.display = "none";
-        }
+    console.log('document');
+    if (!event.btnHandl) {
+        console.log(event);
+        console.log(btnIndex);      
+        activateToogleMarketElements[btnIndex].style.display = "none";
     }
-);
+});
 
 setTimeout(function() {
     stateMarketElement.classList.remove('card-market__state--creating');
