6e5b4552278e6e5abf5aaf38f42e8e9fe94a7e26
katrinLuna <kate.nik2017@yandex.ru>
2019-10-24 11:44:15 +0300

    add toogle-effect for btn and simlify current script

--- build/script.js
+++ build/script.js
@@ -2,28 +2,19 @@ var settingBtnElements = document.querySelectorAll('.card-market__settings');
 var activateToogleMarketElements = document.querySelectorAll('.card-market__toogle-active-btn');
 var stateMarketElement = document.querySelector('.card-market__state--creating');
 
-settingBtnElements.forEach(function(item, i) {
-
+settingBtnElements.forEach(function (item, i) {
     item.addEventListener('click', function (event) {
-        if (activateToogleMarketElements[i].hidden = true) {
-            activateToogleMarketElements[i].hidden = false;
-            event.btnHandl = i;
-        }
+        activateToogleMarketElements[i].toggleAttribute('hidden');
+        event.btnHandl = i;
     });
 });
 
 document.addEventListener('click', function (event) {
-    if (event.btnHandl != null) {
-        activateToogleMarketElements.forEach(function (item, i) {
-            if (i !== event.btnHandl) {
-                item.hidden = true;
-            }
-        });
-    } else {
-        activateToogleMarketElements.forEach(function (item, i) {
+    activateToogleMarketElements.forEach(function (item, i) {
+        if (i != event.btnHandl) {
             item.hidden = true;
-        });
-    }
+        }
+    });
 });
 
 setTimeout(function() {
