69f7b1bed39bfbdf2e60156edfc01954f027b8e7
katrinLuna <kate.nik2017@yandex.ru>
2019-10-24 09:50:20 +0300

    change if-state and display to hidden

--- build/script.js
+++ build/script.js
@@ -2,36 +2,27 @@ var settingBtnElements = document.querySelectorAll('.card-market__settings');
 var activateToogleMarketElements = document.querySelectorAll('.card-market__toogle-active-btn');
 var stateMarketElement = document.querySelector('.card-market__state--creating');
 
-// settingBtnElements.addEventListener('click', function (event) {
-//         if (activateToogleMarketElement.style.display == 'none') {
-//             activateToogleMarketElement.style.display = "block";
-//             event.btnHandl = true;
-//         }
-//     }
-// );
-
 settingBtnElements.forEach(function(item, i) {
 
     item.addEventListener('click', function (event) {
-    // добавить цикл в обработчик который будет скрыть все элементы кроме i
-        
-        if (activateToogleMarketElements[i].style.display == 'none') {
-            activateToogleMarketElements[i].style.display = "block";
-            event.btnHandl = true;            
-            event.bla = 1; //это если у нас два обработчика на одно событие - в плане ловим и на документе это событие - в параметр записываем i элемент и внутри сл обработчика делаем скрытие 
-            // -- если есть это свойство в объекте событие- значит у нас через кнопку событие прошло - и тогда у любого кроме i-элемента скрываем блок, а если нет свойства - то скрываем у всех
-            // можно в  btnHandl  прям класть цифру и ниже проверять, что тут нет null 
-            console.log(event);
+        if (activateToogleMarketElements[i].hidden = true) {
+            activateToogleMarketElements[i].hidden = false;
+            event.btnHandl = i;
         }
     });
 });
 
 document.addEventListener('click', function (event) {
-    console.log('document');
-    if (!event.btnHandl) {
-        console.log(event);
-        console.log(btnIndex);      
-        activateToogleMarketElements[btnIndex].style.display = "none";
+    if (event.btnHandl != null) {
+        activateToogleMarketElements.forEach(function (item, i) {
+            if (i !== event.btnHandl) {
+                item.hidden = true;
+            }
+        });
+    } else {
+        activateToogleMarketElements.forEach(function (item, i) {
+            item.hidden = true;
+        });
     }
 });
 
